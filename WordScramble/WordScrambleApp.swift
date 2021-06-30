0a904473c8e1d4e2080f5904b960571943286ac9
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-06-30 09:50:22 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bruce Gilmour on 2021-06-30.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
